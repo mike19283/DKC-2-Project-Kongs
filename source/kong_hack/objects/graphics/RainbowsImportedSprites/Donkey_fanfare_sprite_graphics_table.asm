73bdb8b5977b34e3c416745b0fa18d8393f55c70
Michael Mingrone (RainbowSprinklez) <mikemingrone@gmail.com>
2024-08-12 08:07:05 -0400

    Sprite
    See if it works

---
+++ source/kong_hack/objects/graphics/RainbowsImportedSprites/Donkey_fanfare_sprite_graphics_table.asm
@@ -0,0 +1,5 @@
+dl Donkey_008C_D8B1F0 : db $00			;4818
+dl Donkey_0090_D8B46C : db $00			;481C
+dl Donkey_0094_D8B6E8 : db $00			;4820
+dl Donkey_0098_D8B964 : db $00			;4824
+dl Donkey_009C_D8BBE0 : db $00			;4828
---
+++ source/kong_hack/objects/graphics/RainbowsImportedSprites/data/Donkey_fanfare_data.asm
@@ -0,0 +1,10 @@
+Donkey_008C_D8B1F0:
+	incbin "bin/Donkey_008C_D8B1F0.bin"
+Donkey_0090_D8B46C:
+	incbin "bin/Donkey_0090_D8B46C.bin"
+Donkey_0094_D8B6E8:
+	incbin "bin/Donkey_0094_D8B6E8.bin"
+Donkey_0098_D8B964:
+	incbin "bin/Donkey_0098_D8B964.bin"
+Donkey_009C_D8BBE0:
+	incbin "bin/Donkey_009C_D8BBE0.bin"
